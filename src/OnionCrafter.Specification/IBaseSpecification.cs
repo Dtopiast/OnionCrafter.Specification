511712f05fb791d785016e5378b01db262444cbd
dtopiast <dtopiast@protonmail.com>
2023-05-03 21:59:46 -0600

    Add IWriteRepository

--- src/OnionCrafter.Specification/IBaseSpecification.cs
+++ src/OnionCrafter.Specification/IBaseSpecification.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OnionCrafter.Specification
 {
-    public class IBaseSpecification
+    public interface IBaseSpecification
     {
     }
 }
--- src/OnionCrafter.Specification/IWriteRepository.cs
+++ src/OnionCrafter.Specification/IWriteRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OnionCrafter.Specification
 {
-    public interface IReadRepository
+    internal interface IWriteRepository
     {
     }
 }
