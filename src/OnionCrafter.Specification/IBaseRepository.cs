b0c2158d549857eb446da06d06febe9c54d260d0
dtopiast <dtopiast@protonmail.com>
2023-05-03 22:17:03 -0600

    Addition of generic TEntity in IBaseRepository with the restriction TEntity : IBaseEntity

--- src/OnionCrafter.Specification/IBaseRepository.cs
+++ src/OnionCrafter.Specification/IBaseRepository.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OnionCrafter;
+using OnionCrafter.Base.Entities;
 
 namespace OnionCrafter.Specification
 {
-    public interface IBaseRepository : IDisposable
+    public interface IBaseRepository<TEntity> : IDisposable where TEntity : IBaseEntity
     {
     }
 }
