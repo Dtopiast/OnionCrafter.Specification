c4612aceeb0ee1bf86a17757fabd351b3868504a
dtopiast <dtopiast@protonmail.com>
2023-05-06 16:13:24 -0600

    Added basic methods in IReadRepository interface for read operations

--- src/OnionCrafter.Specification/IReadRepository.cs
+++ src/OnionCrafter.Specification/IReadRepository.cs
@@ -4,5 +4,20 @@ namespace OnionCrafter.Specification
 {
     public interface IReadRepository<TEntity, TKey> : IBaseRepository<TEntity> where TEntity : IEntity<TKey>
     {
+        Task<TEntity?> GetByIdAsync(TKey id, CancellationToken cancellationToken = default);
+
+        Task<TEntity?> GetFirstOrDefaultAsync(ISpecification<TEntity> specification, CancellationToken cancellationToken = default);
+
+        Task<TEntity?> GetSingleOrDefaultAsync(ISpecification<TEntity> specification, CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+
+        Task<IEnumerable<TEntity>> GetAllAsync(ISpecification<TEntity> specification, CancellationToken cancellationToken = default);
+
+        Task<int> CountAsync(CancellationToken cancellationToken = default);
+
+        Task<bool> AnyAsync(ISpecification<TEntity> specification, CancellationToken cancellationToken = default);
+
+        Task<bool> AnyAsync(CancellationToken cancellationToken = default);
     }
 }
