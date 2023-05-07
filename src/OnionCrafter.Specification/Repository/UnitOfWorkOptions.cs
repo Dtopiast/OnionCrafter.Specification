43016e473e8367a9ea3da1097089f273e4a32fff
dtopiast <dtopiast@protonmail.com>
2023-05-07 15:38:29 -0600

    Addition of options for log messages

--- src/OnionCrafter.Specification/Repository/UnitOfWorkOptions.cs
+++ src/OnionCrafter.Specification/Repository/UnitOfWorkOptions.cs
@@ -11,5 +11,8 @@ namespace OnionCrafter.Specification.Repository
     public class UnitOfWorkOptions : IServiceOptions
     {
         public bool UseLogger { get; set; }
+        public string? RollbackMessageLogger { get; set; }
+        public string? CommitMessageLogger { get; set; }
+        public string? BeginMessageLogger { get; set; }
     }
 }
