795e365494691e541b11e915f7945ff5c6dc1be0
dtopiast <dtopiast@protonmail.com>
2023-05-07 13:36:02 -0600

    Creeation of UnitOfWork

---
+++ src/OnionCrafter.Specification/Repository/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCrafter.Specification.Repository
+{
+    public class UnitOfWork
+    {
+    }
+}
