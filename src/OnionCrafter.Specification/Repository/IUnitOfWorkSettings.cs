a86694d27b26f42344ad41f32258923a9b9558fc
dtopiast <dtopiast@protonmail.com>
2023-05-07 14:48:29 -0600

    Creation and basic implementation of IUnitOfWorkSettings

---
+++ src/OnionCrafter.Specification/Repository/IUnitOfWorkSettings.cs
@@ -0,0 +1,15 @@
+﻿using OnionCrafter.Base.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCrafter.Specification.Repository
+{
+    //IServiceOptions debe de tener tipado
+    public class IUnitOfWorkSettings : IServiceOptions
+    {
+        public bool UseLogger { get; set; }
+    }
+}
