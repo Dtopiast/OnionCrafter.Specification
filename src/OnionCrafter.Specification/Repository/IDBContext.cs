93dc09c2e1f6f404adb8faad2bb0fcab9aeb2f77
dtopiast <dtopiast@protonmail.com>
2023-05-07 14:12:34 -0600

    Basic implementation of IDBContext

--- src/OnionCrafter.Specification/Repository/IDBContext.cs
+++ src/OnionCrafter.Specification/Repository/IDBContext.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using OnionCrafter.Base.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,14 @@ namespace OnionCrafter.Specification.Repository
 {
     public interface IDBContext : IDisposable
     {
+        public DbSet<TEntity> Set<TEntity>() where TEntity : class, IBaseEntity;
+
+        public Task<int> SaveChangesAsync(CancellationToken cancellationTaken = new CancellationToken());
+
+        public EntityEntry<TEntity> Update<TEntity>(TEntity entity) where TEntity : class, IBaseEntity;
+
+        public EntityEntry<TEntity> Remove<TEntity>(TEntity entity) where TEntity : class, IBaseEntity;
+
+        public ValueTask<EntityEntry<TEntity>> AddAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default) where TEntity : class, IBaseEntity;
     }
 }
