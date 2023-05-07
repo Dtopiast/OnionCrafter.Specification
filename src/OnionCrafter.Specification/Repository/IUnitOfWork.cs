fa36d3d62efe279c87eea8decebd033ce8e5155f
dtopiast <dtopiast@protonmail.com>
2023-05-07 13:56:58 -0600

    Creation of the IUnitOfWork interface

---
+++ src/OnionCrafter.Specification/Repository/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCrafter.Specification.Repository
+{
+    public interface IUnitOfWork
+    {
+        Task<int> CommitAsync();
+    }
+
+    public interface IUnitOfWork<TDBContext> : IUnitOfWork
+    {
+        void RollbackAsync();
+    }
+}
--- src/OnionCrafter.Specification/Repository/UnitOfWork.cs
+++ src/OnionCrafter.Specification/Repository/UnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnionCrafter.Base.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace OnionCrafter.Specification.Repository
 {
-    public class UnitOfWork
+    public class UnitOfWork : IRepositoryFactory
     {
     }
 }
