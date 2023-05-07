c9270a20c1043a0562de72886857d1494e2e3596
Rafael Ramirez <102709670+Dtopiast@users.noreply.github.com>
2023-05-06 19:31:31 -0600

    Basic implementation of ISpecification  (#6)
    * Addition of basic structure in ISpecification
    
    * Update ISpecification properties to nullable
    
    * Implementation of ISpecification in its base class BaseSpecification
    
    ---------
    
    Co-authored-by: dtopiast <dtopiast@protonmail.com>

--- src/OnionCrafter.Specification/ISpecification.cs
+++ src/OnionCrafter.Specification/ISpecification.cs
@@ -1,8 +1,13 @@
 ﻿using OnionCrafter.Base.Entities;
+using System.Linq.Expressions;
 
 namespace OnionCrafter.Specification
 {
-    public interface ISpecification<T> where T : IBaseEntity
+    public interface ISpecification<Entity> where Entity : IBaseEntity
     {
+        Expression<Func<Entity, bool>>? Criteria { get; }
+        List<Expression<Func<Entity, object>>> Includes { get; }
+        Expression<Func<Entity, object>>? OrderBy { get; }
+        Expression<Func<Entity, object>>? OrderByDescending { get; }
     }
 }
