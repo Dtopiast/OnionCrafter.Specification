c3b172cb5fe04db5afc11041e48210214618f175
Dtopiast <dtopiast@protonmail.com>
2023-05-08 10:08:21 -0600

    Implementation of the IUnitOfWork interface in UnitOfWork

--- src/OnionCrafter.Specification/Repository/UnitOfWork.cs
+++ src/OnionCrafter.Specification/Repository/UnitOfWork.cs
@@ -10,5 +10,40 @@ namespace OnionCrafter.Specification.Repository
     public class UnitOfWork<TDBContext> : IRepositoryFactory, IUnitOfWork<TDBContext>
         where TDBContext : IDBContext
     {
+        private readonly ILogger<UnitOfWork<TDBContext>>? _logger;
+        private readonly UnitOfWorkOptions _options;
+        private readonly TDBContext _context;
+
+        public UnitOfWork(TDBContext context, IOptions<UnitOfWorkOptions> options, ILogger<UnitOfWork<TDBContext>>? logger)
+        {
+            _logger = logger;
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _options = options.Value;
+            if (_logger == null && _options.UseLogger)
+                throw new ArgumentNullException("No loggers are registered, if your project does not require logging set the UseLogger option to false in your implementation.");
+        }
+
+        public async Task BeginAsync()
+        {
+            await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task<int> CommitAsync()
+        {
+            await _context.Database.CommitTransactionAsync();
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task RollbackAsync()
+        {
+            await _context.Database.RollbackTransactionAsync();
+        }
+
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
     }
 }
