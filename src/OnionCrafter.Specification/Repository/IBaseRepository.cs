79d1dc77fda11cdffa1869075744518a983d22bd
Dtopiast <dtopiast@protonmail.com>
2023-05-08 09:25:04 -0600

    Addition of the RepositoryPrivilegesType enumeration to implement a RepositoryPrivileges property in IBaseRepository to know the privileges of its children.

--- src/OnionCrafter.Specification/Repository/IBaseRepository.cs
+++ src/OnionCrafter.Specification/Repository/IBaseRepository.cs
@@ -8,7 +8,15 @@ using OnionCrafter.Base.Entities;
 
 namespace OnionCrafter.Specification
 {
+    public enum RepositoryPrivilegesType
+    {
+        None = 0,
+        Write,
+        Read,
+        Complete
+    }
     public interface IBaseRepository : IDisposable
     {
+        public RepositoryPrivilegesType RepositoryPrivileges { get; }
     }
 }
