c248f5365a0ca5fc2d92596dc2e88191aa8ab698
dtopiast <dtopiast@protonmail.com>
2023-05-07 23:06:28 -0600

    Delete of the property AllowCreationOfGenericRepositoryToAnyEntity to create AutomaticallyRegisterRepositories

--- src/OnionCrafter.Specification/Repository/UnitOfWorkOptions.cs
+++ src/OnionCrafter.Specification/Repository/UnitOfWorkOptions.cs
@@ -11,8 +11,7 @@ namespace OnionCrafter.Specification.Repository
     public class UnitOfWorkOptions : IServiceOptions
     {
         public bool UseLogger { get; set; }
-        public bool AllowCreationOfGenericRepositoryToAnyEntity { get; set; }
-        public bool AutomaticallyLoadRepositories { get; set; }
+        public bool AutomaticallyRegisterRepositories { get; set; }
         public string? RollbackMessageLogger { get; set; }
         public string? CommitMessageLogger { get; set; }
         public string? BeginMessageLogger { get; set; }
