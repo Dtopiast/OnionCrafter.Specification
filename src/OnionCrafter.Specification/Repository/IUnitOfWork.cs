1af9f1d85b8bdd2bde25b14170f384b0152ae38e
Dtopiast <dtopiast@protonmail.com>
2023-05-08 22:32:31 -0600

    Change of return property in IUnitOfWork CommitAsync method

--- src/OnionCrafter.Specification/Repository/IUnitOfWork.cs
+++ src/OnionCrafter.Specification/Repository/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnionCrafter.Base.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,9 @@ using System.Threading.Tasks;
 
 namespace OnionCrafter.Specification.Repository
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IService<UnitOfWorkOptions>
     {
-        Task<int> CommitAsync();
+        Task<bool> CommitAsync();
     }
 
     public interface IUnitOfWork<TDBContext> : IUnitOfWork
