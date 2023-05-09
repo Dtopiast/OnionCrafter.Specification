a978d02b842bc0e4f8b1c18e4f763357381ed306
Dtopiast <dtopiast@protonmail.com>
2023-05-08 21:09:25 -0600

    Addition of methods for repository management in IRepositoryContainer

--- src/OnionCrafter.Specification/Repository/Cache/IRepositoryContainer.cs
+++ src/OnionCrafter.Specification/Repository/Cache/IRepositoryContainer.cs
@@ -1,13 +1,22 @@
-﻿using OnionCrafter.Base.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OnionCrafter.Base.Entities;
+using OnionCrafter.Base.Services;
 
 namespace OnionCrafter.Specification.Repository.Cache
 {
-    public class IRepositoryContainer : IService
+    public interface IRepositoryContainer : IService<RepositoryContainerOptions>
     {
+        public Task<TRepository> GetOrCreateRepositoryAsync<TEntity, TKey, TRepository>()
+            where TEntity : IEntity<TKey>
+            where TRepository : IBaseRepository;
+
+        public Task<int> CountAsync();
+
+        public Task<bool> ContainsRepositoryAsync<TEntity, TKey, TRepository>(TRepository repository)
+            where TEntity : IEntity<TKey>
+            where TRepository : IBaseRepository;
+
+        public Task<bool> RemoveRepositoryAsync<TEntity, TKey, TRepository>(TRepository repository)
+            where TEntity : IEntity<TKey>
+            where TRepository : IBaseRepository;
     }
 }
