3e81fd2ec3385233dd0501899853c76de98bb9d7
Dtopiast <dtopiast@protonmail.com>
2023-05-08 20:54:41 -0600

    Addition of basic option properties in RepositoryContainerOptions

---
+++ src/OnionCrafter.Specification/Repository/Cache/RepositoryContainerOptions.cs
@@ -0,0 +1,15 @@
+﻿using OnionCrafter.Base.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCrafter.Specification.Repository.Cache
+{
+    public class RepositoryContainerOptions : IServiceOptions
+    {
+        public bool UseLogger { get; set; }
+        public bool AutomaticallyRegisterRepositories { get; set; }
+    }
+}
