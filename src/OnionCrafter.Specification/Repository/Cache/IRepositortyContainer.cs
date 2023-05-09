f5fa833e3670e679af9803479a9b98e7cbd9ea13
Dtopiast <dtopiast@protonmail.com>
2023-05-08 20:30:59 -0600

    Creation and empty implementation of RepositoryContainer in a new folder named cache

---
+++ src/OnionCrafter.Specification/Repository/Cache/IRepositortyContainer.cs
@@ -0,0 +1,13 @@
+﻿using OnionCrafter.Base.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCrafter.Specification.Repository.Cache
+{
+    public class IRepositortyContainer : IService
+    {
+    }
+}
---
+++ src/OnionCrafter.Specification/Repository/Cache/RepositoryContainer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCrafter.Specification.Repository.Cache
+{
+    public class RepositoryContainer : IRepositortyContainer
+    {
+        public RepositoryContainer()
+        {
+        }
+    }
+}
