d51e2ad933487aa1c81566402fcd6c7869aaea68
Dtopiast <dtopiast@protonmail.com>
2023-05-08 22:28:20 -0600

    Implementation of the CreateInformationOrErrorLog method

--- src/OnionCrafter.Specification/Utils/LoggingUtils.cs
+++ src/OnionCrafter.Specification/Utils/LoggingUtils.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,23 @@ using System.Threading.Tasks;
 
 namespace OnionCrafter.Specification.Utils
 {
-    public class LoggingUtils
+    public static class LoggingUtils
     {
+        public static void CreateInformationOrErrorLog(this ILogger logger, bool result, string successfullMessage, string errorMessage, string? obj = null, params object?[] args)
+        {
+            //usa string builder
+            if (result)
+            {
+                if (obj != null)
+                    successfullMessage = obj + " " + successfullMessage;
+                logger.LogInformation(successfullMessage, args);
+            }
+            else
+            {
+                if (obj != null)
+                    errorMessage = obj + " " + errorMessage;
+                logger.LogError(errorMessage, args);
+            }
+        }
     }
 }
