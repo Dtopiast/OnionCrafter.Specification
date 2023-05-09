8ff198ee35660231be13c2a5642ebabb57811ce0
Dtopiast <dtopiast@protonmail.com>
2023-05-09 08:35:59 -0600

    Implementation of the AddRepositoryFactory method in ServiceExtensions to add repository access and cache logic.

--- src/OnionCrafter.Specification/DependencyInjection/ServiceExtensions.cs
+++ src/OnionCrafter.Specification/DependencyInjection/ServiceExtensions.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
+using OnionCrafter.Base.Services;
+using OnionCrafter.Specification.Repository;
+using OnionCrafter.Specification.Repository.Cache;
 
 namespace OnionCrafter.Specification.DependencyInjection
 {
     public static class ServiceExtensions
     {
+        public static IServiceCollection AddRepositoryFactory(this IServiceCollection services, Action<UnitOfWorkOptions> unitOfWorkOptions, Action<RepositoryContainerOptions> repositoryContainerOptions)
+        {
+            services.AddTypedSingletonWithOptions<RepositoryContainerOptions>(typeof(IRepositoryContainer),typeof(RepositoryContainer), repositoryContainerOptions);
+            services.AddTypedSingletonWithOptions<UnitOfWorkOptions>(typeof(IUnitOfWork<>),typeof(UnitOfWork<>), unitOfWorkOptions );
+            return services;
+        }
+        public static IServiceCollection AddRepositoryFactory(this IServiceCollection services, Action<UnitOfWorkOptions> unitOfWorkOptions)
+        {
+            services.AddTypedSingleton(typeof(IRepositoryContainer), typeof(RepositoryContainer));
+            services.AddTypedSingletonWithOptions<UnitOfWorkOptions>(typeof(IUnitOfWork<>), typeof(UnitOfWork<>), unitOfWorkOptions);
+            return services;
+        }
+        public static IServiceCollection AddRepositoryFactory(this IServiceCollection services)
+        {
+            services.AddTypedSingleton(typeof(IRepositoryContainer), typeof(RepositoryContainer));
+            services.AddTypedSingleton(typeof(IUnitOfWork<>), typeof(UnitOfWork<>));
+            return services;
+        }
     }
 }
