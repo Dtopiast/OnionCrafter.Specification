bf3649c7e7a7539706a2abc1b13c3510fa431a96
Dtopiast <dtopiast@protonmail.com>
2023-05-09 23:26:23 -0600

    Modification of the operation of the methods and their return values of IDBContext, in addition to being implemented in IBaseContext.

--- src/OnionCrafter.Specification/Context/IDBContext.cs
+++ src/OnionCrafter.Specification/Context/IDBContext.cs
@@ -5,18 +5,7 @@ using OnionCrafter.Base.Entities;
 
 namespace OnionCrafter.Specification.Context
 {
-    public interface IDBContext : IDisposable
+    public interface IDBContext : IBaseContext, IDisposable
     {
-        public DatabaseFacade Database { get; }
-
-        public DbSet<TEntity> Set<TEntity>() where TEntity : class, IBaseEntity;
-
-        public Task<int> SaveChangesAsync(CancellationToken cancellationTaken = new CancellationToken());
-
-        public EntityEntry<TEntity> Update<TEntity>(TEntity entity) where TEntity : class, IBaseEntity;
-
-        public EntityEntry<TEntity> Remove<TEntity>(TEntity entity) where TEntity : class, IBaseEntity;
-
-        public ValueTask<EntityEntry<TEntity>> AddAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default) where TEntity : class, IBaseEntity;
     }
 }
