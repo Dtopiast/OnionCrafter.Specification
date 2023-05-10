02be4d9f265c6629c32fac5c695c647e83b32282
Dtopiast <dtopiast@protonmail.com>
2023-05-09 22:13:52 -0600

    Change of the return type in IWriteRepository methods

--- src/OnionCrafter.Specification/Repository/IWriteRepository.cs
+++ src/OnionCrafter.Specification/Repository/IWriteRepository.cs
@@ -4,16 +4,16 @@ namespace OnionCrafter.Specification.Repository
 {
     public interface IWriteRepository<TEntity, TKey> : IBaseRepository where TEntity : IEntity<TKey>
     {
-        Task<TEntity> CreateAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task CreateAsync(TEntity entity, CancellationToken cancellationToken = default);
 
-        Task<IEnumerable<TEntity>> CreateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+        Task CreateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
 
-        Task<bool> UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task UpdateAsync(TEntity entity, CancellationToken cancellationToken = default);
 
-        Task<bool> UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+        Task UpdateRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
 
-        Task<bool> RemoveAsync(TEntity entity, CancellationToken cancellationToken = default);
+        Task RemoveAsync(TEntity entity, CancellationToken cancellationToken = default);
 
-        Task<bool> RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+        Task RemoveRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
     }
 }
