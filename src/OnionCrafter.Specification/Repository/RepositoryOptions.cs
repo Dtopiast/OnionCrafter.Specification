aad6824cb7e19ccb6bfb24a9f3ba6e4e7580ddd2
Dtopiast <dtopiast@protonmail.com>
2023-05-10 21:14:25 -0600

    Creation of the RepositoryOptions class

---
+++ src/OnionCrafter.Specification/Repository/RepositoryOptions.cs
@@ -0,0 +1,16 @@
+﻿using OnionCrafter.Base.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionCrafter.Specification.Repository
+{
+    public class RepositoryOptions : IServiceOptions
+    {
+        public RepositoryPrivilegesType RepositoryPrivileges { get; }
+        public RepositoryOriginType RepositoryOrigin { get; }
+        public string? RepositoryName { get; }
+    }
+}
